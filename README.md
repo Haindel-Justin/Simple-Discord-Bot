7638ee3440a9827b6e54c0937f709554b5e7db30
Justin Haindel <33536267+Haindel-Justin@users.noreply.github.com>
2019-08-06 20:20:39 -0500

    clear command

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 1. Features
     * Auto addition of roles
+    * Clear command
+        * Default value of 10
 2. How to add to your server
 
 That's it, literally all the bot does at the moment.
--- bot.py
+++ bot.py
@@ -9,9 +9,6 @@ async def on_ready():
 
 @client.command()
 async def clear(ctx, amount=10):
-    if amount != -1:
-        await ctx.channel.purge(limit=amount)
-    else:
-        await ctx.channel.purge(limit=1000)
+    await ctx.channel.purge(limit=amount)
 
 client.run('NjA4NDIwODM0NjgzMzg3OTQ0.XUocEA.lbFEGHgB08aWWLTaKGFv0hR7KCY')
