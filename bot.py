9f3d5e87d0243bc108f631f6b0a4f4e54eb1fc86
BuildTools <jacobsenscott07@gmail.com>
2019-08-06 21:48:16 -0500

    Add addrole command and default role addition for new users

--- bot.py
+++ bot.py
@@ -11,4 +11,17 @@ async def on_ready():
 async def clear(ctx, amount=10):
     await ctx.channel.purge(limit=amount)
 
+@client.command()
+async def addrole(ctx, role: discord.Role, member: discord.Member=None):
+    member = member or ctx.message.author
+    await member.add_roles(role)
+
+@client.event
+async def on_member_join(member):
+    guild = client.get_guild(608418023748927508);
+    for role in guild.roles:
+        if (role.name == 'Default'):
+            default_role = role
+    await member.add_roles(default_role)
+
 client.run('NjA4NDIwODM0NjgzMzg3OTQ0.XUocEA.lbFEGHgB08aWWLTaKGFv0hR7KCY')
